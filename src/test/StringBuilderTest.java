99c216327f3d3a41266d3140288393425c67e811
yutao <1185651333@qq.com>
2018-05-10 20:24:13 +0800

    ..

--- src/test/StringBuilderTest.java
+++ src/test/StringBuilderTest.java
@@ -3,9 +3,10 @@ package test;
 public class StringBuilderTest {
 
 	public static void main(String[] args) {
-		String result = "";
+		String result = "bb";
 		for(int i=0; i<20;i++){
 			result += "a";
+			new StringBuilder().append(result);
 		}
 		System.out.println(result);
 	}
